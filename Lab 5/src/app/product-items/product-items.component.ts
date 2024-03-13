91c51e080f444ba6c09e884d1c387f8a42c3381b
progermyn <123311974+progermyn@users.noreply.github.com>
2024-03-13 21:31:50 +0600

    Add files via upload

---
+++ Lab 5/src/app/product-items/product-items.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { products } from '../products'
+import { Product } from '../products'
+
+@Component({
+  selector: 'app-product-items',
+  templateUrl: './product-items.component.html',
+  styleUrl: './product-items.component.css'
+})
+export class ProductItemsComponent {
+  @Input () product: Product | undefined;
+  @Input () a: any;
+  @Output () remove = new EventEmitter();
+  isLike = false;
+
+  share(link:string) {
+    const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(link)}`;
+    window.open(telegramUrl, '_blank');
+  }
+  nextImage(a:number){
+    if(products[a].i < 2){
+      products[a].i += 1;
+    }
+    else{
+      products[a].i = 0;
+    }
+  }
+  deleteProduct(){
+    this.remove.emit(this.product?.id);
+  }
+  like(){
+    this.isLike = !this.isLike;
+    if(this.isLike && this.product){
+      this.product.like++;
+    }
+    else if(this.product){
+      this.product.like--;
+    }
+  }
+}
